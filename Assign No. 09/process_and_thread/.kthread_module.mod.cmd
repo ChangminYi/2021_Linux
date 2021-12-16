6bc459f23424061e645cd693556eb1031f741c62
ChangminYi <jerry9803@gmail.com>
2021-12-16 20:16:29 +0900

    Assignment No. 09 added

---
+++ Assign No. 09/process_and_thread/.kthread_module.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/yichangmin/바탕화면/process_and_thread/kthread_module.mod := { echo  /home/yichangmin/바탕화면/process_and_thread/kthread_module.o; echo; } > /home/yichangmin/바탕화면/process_and_thread/kthread_module.mod
---
+++ Assign No. 09/process_and_thread/kthread_module.mod
@@ -0,0 +1,2 @@
+/home/yichangmin/바탕화면/process_and_thread/kthread_module.o
+
